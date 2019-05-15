135936c959a76671a8b4f5ed1af0b0b76b5a650b
andurill <gowtham.jn@gmail.com>
2019-05-15 12:02:15 -0400

    update tests

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,4 +1,4 @@
-'''
+"""
 MiMSI Test Script 
 
 This testing script runs the full pipeline for the 100x and 200x model on an example tumor/normal
@@ -15,7 +15,7 @@ zieglerj@mskcc.org
 and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, 
 either version 3 or later. See the LICENSE file for details
 
-'''
+"""
 
 
 import numpy as np
@@ -31,13 +31,15 @@ import os
 import gzip
 import shutil
 
-from data.generate_vectors.create_data import main as create_data
-from main.evaluate_sample import main as run_eval
+from data.generate_vectors.create_data import create_data
+from main.evaluate_sample import run_eval
 
+# Global variables
 vector_folder = "./example"
 tumor_bam = "./example.tumor.bam"
 normal_bam = "./example.normal.bam"
 ms_list = "./microsatellites.list"
+
 
 def setup():
     os.mkdir(vector_folder)
@@ -58,24 +60,52 @@ def teardown():
 def test_100x_model():
 
     # test data creation
-    create_data(None, tumor_bam, normal_bam, "example", ms_list, vector_folder, False, 50, 16)
+    create_data(
+        None, tumor_bam, normal_bam, "example", ms_list, vector_folder, False, 50, 16
+    )
 
     assert os.path.isfile("./example/example_-1_data.npy")
     assert os.path.isfile("./example/example_-1_locations.npy")
 
-    run_eval("../model/mimsi_mskcc_impact.model", vector_folder, False, 2, True, "example", 50)
+    run_eval(
+        "../model/mimsi_mskcc_impact.model",
+        vector_folder,
+        False,
+        2,
+        True,
+        "both",
+        vector_folder,
+        "example",
+        50,
+        0.95,
+    )
 
-    assert os.path.isfile("./example_results.npy")
+    assert os.path.isfile("./example/example_results.npy")
+    assert os.path.isfile("./example/example_results.txt")
 
 
 def test_200x_model():
 
     # test data creation
-    create_data(None, tumor_bam, normal_bam, "example", ms_list, vector_folder, False, 100, 16)
+    create_data(
+        None, tumor_bam, normal_bam, "example", ms_list, vector_folder, False, 100, 16
+    )
 
     assert os.path.isfile("./example/example_-1_data.npy")
     assert os.path.isfile("./example/example_-1_locations.npy")
 
-    run_eval("../model/mimsi_mskcc_impact_200.model", vector_folder, False, 2, True, "example", 100)
+    run_eval(
+        "../model/mimsi_mskcc_impact_200.model",
+        vector_folder,
+        False,
+        2,
+        True,
+        "both",
+        vector_folder,
+        "example",
+        100,
+        0.95,
+    )
 
-    assert os.path.isfile("./example_results.npy")
+    assert os.path.isfile("./example/example_results.npy")
+    assert os.path.isfile("./example/example_results.txt")
