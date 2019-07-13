d693c8548ee2af12449a3549018396dd78cfa8cb
andurill <gowtham.jn@gmail.com>
2019-07-13 14:18:00 -0400

    :sparkles: update test functions and also add new test function for batch test

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -19,6 +19,7 @@ either version 3 or later. See the LICENSE file for details
 
 
 import numpy as np
+import pandas as pd
 import argparse
 import torch
 import torch.utils.data as data_utils
@@ -34,10 +35,11 @@ from data.generate_vectors.create_data import create_data
 from main.evaluate_sample import run_eval
 
 # Global variables
-vector_folder = "./example"
-tumor_bam = "./example.tumor.bam"
-normal_bam = "./example.normal.bam"
+vector_folder = "./test_dir"
+tumor_bam = "./test.tumor.bam"
+normal_bam = "./test.normal.bam"
 ms_list = "./microsatellites.list"
+case_list = "./test_case_list.txt"
 
 
 def setup():
@@ -52,19 +54,19 @@ def teardown():
 
     os.rmdir(vector_folder)
 
-    if os.path.isfile("./example_results.npy"):
-        os.unlink("./example_results.npy")
+    # if os.path.isfile("./test_results.npy"):
+    #     os.unlink("./test_results.npy")
 
 
 def test_100x_model():
 
     # test data creation
     create_data(
-        None, tumor_bam, normal_bam, "example", ms_list, vector_folder, False, 50, 16
+        ms_list, vector_folder, 50, 16, None, tumor_bam, normal_bam, "tumor", "normal"
     )
 
-    assert os.path.isfile("./example/example_-1_data.npy")
-    assert os.path.isfile("./example/example_-1_locations.npy")
+    assert os.path.isfile("./test_dir/tumor_normal_-1_data.npy")
+    assert os.path.isfile("./test_dir/tumor_normal_-1_locations.npy")
 
     run_eval(
         "../model/mimsi_mskcc_impact.model",
@@ -74,24 +76,24 @@ def test_100x_model():
         True,
         "both",
         vector_folder,
-        "example",
+        "test-run",
         50,
         0.95,
     )
 
-    assert os.path.isfile("./example/example_results.npy")
-    assert os.path.isfile("./example/example_results.txt")
+    assert os.path.isfile("./test_dir/tumor_normal_results.npy")
+    assert os.path.isfile("./test_dir/test-run_results.txt")
 
 
 def test_200x_model():
 
     # test data creation
     create_data(
-        None, tumor_bam, normal_bam, "example", ms_list, vector_folder, False, 100, 16
+        ms_list, vector_folder, 100, 16, None, tumor_bam, normal_bam, "tumor", "normal"
     )
 
-    assert os.path.isfile("./example/example_-1_data.npy")
-    assert os.path.isfile("./example/example_-1_locations.npy")
+    assert os.path.isfile("./test_dir/tumor_normal_-1_data.npy")
+    assert os.path.isfile("./test_dir/tumor_normal_-1_locations.npy")
 
     run_eval(
         "../model/mimsi_mskcc_impact_200.model",
@@ -101,10 +103,41 @@ def test_200x_model():
         True,
         "both",
         vector_folder,
-        "example",
+        "test-run",
         100,
         0.95,
     )
 
-    assert os.path.isfile("./example/example_results.npy")
-    assert os.path.isfile("./example/example_results.txt")
+    assert os.path.isfile("./test_dir/tumor_normal_results.npy")
+    assert os.path.isfile("./test_dir/test-run_results.txt")
+
+
+def batch_test():
+
+    # test data creation
+    create_data(ms_list, vector_folder, 50, 16, case_list, None, None, None, None)
+
+    assert os.path.isfile("./test_dir/tumor1_normal1_-1_data.npy")
+    assert os.path.isfile("./test_dir/tumor1_normal1_-1_locations.npy")
+    assert os.path.isfile("./test_dir/tumor2_normal2_-1_data.npy")
+    assert os.path.isfile("./test_dir/tumor2_normal2_-1_locations.npy")
+    assert os.path.isfile("./test_dir/tumor3_-1_data.npy")
+    assert os.path.isfile("./test_dir/tumor3_-1_locations.npy")
+
+    run_eval(
+        "../model/mimsi_mskcc_impact.model",
+        vector_folder,
+        False,
+        2,
+        True,
+        "both",
+        vector_folder,
+        "test-run",
+        50,
+        0.95,
+    )
+
+    assert os.path.isfile("./test_dir/tumor1_normal1_results.npy")
+    assert os.path.isfile("./test_dir/tumor2_normal2_results.npy")
+    assert os.path.isfile("./test_dir/tumor3_results.npy")
+    assert os.path.isfile("./test_dir/test-run_results.txt")
